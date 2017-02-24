83a36a22d85a31e59627427c30b9f90e48530de2
Kevin Masaryk <kevin.masaryk@gmail.com>
2017-02-23 16:16:30 -0800

    Initial add.

---
+++ README.md
@@ -0,0 +1,16 @@
+# pylogger
+CLI command to generate and send well-formed syslog messages to a syslog receiver.
+Follows arg names as found in the *nix 'logger' command.
+
+## Message format
+`<PRI>TIMESTAMP HOSTNAME TAG: MESSAGE`
+
+This format reflects [RFC3164](http://www.ietf.org/rfc/rfc3164.txt).
+
+## Example Usage
+
+`pylogger.py --server 1.2.3.4 --tag "mytag" "Soylent Green is people!"`
+
+You can see a list of CLI args and descriptions with --help:
+
+`pylogger.py --help`
---
+++ pylogger.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+#-------------------------------------------------------------------------------
+#
+# Name: pylogger.py
+# Description: Send syslog formatted messages to a syslog receiver. Follows
+#              arg names as found in the *nix 'logger' command.
+#
+# Message format: <PRI>TIMESTAMP HOSTNAME TAG: MESSAGE
+#                 This format reflects RFC3164.
+#
+#-------------------------------------------------------------------------------
+
+import argparse
+import logging
+import logging.handlers
+import socket
+import getpass
+
+parser = argparse.ArgumentParser(__file__,
+                                 description="Send a syslog formatted message to a syslog receiver")
+
+# TODO #
+# Need to add udp and tcp options
+
+parser.add_argument("--server",
+                    "-n",
+                    default="localhost",
+                    help="Write to this remote server")
+
+parser.add_argument("--port",
+                    "-P",
+                    type=int,
+                    default=514,
+                    help="Use this port for UDP or TCP connection")
+
+parser.add_argument("--priority",
+                    "-p",
+                    default="DEBUG",
+                    help="The syslog message log level: CRITICAL, ERROR, WARNING, INFO or DEBUG")
+
+parser.add_argument("--tag",
+                    "-t",
+                    help="Mark the message with this tag")
+
+parser.add_argument("msg",
+                    help="The syslog message")
+
+
+# This needs to be rewritten for the syslog handler
+def string_to_level(log_level):
+    """ Convert a commandline string to a proper log level
+    @param string log_level     command line log level argument
+    @return logging.LEVEL       the logging.LEVEL object to return
+    """
+    if log_level == "CRITICAL":
+        return logging.CRITICAL
+    if log_level == "ERROR":
+        return logging.ERROR
+    if log_level == "WARNING":
+        return logging.WARNING
+    if log_level == "INFO":
+        return logging.INFO
+    if log_level == "DEBUG":
+        return logging.DEBUG
+    return logging.NOTSET
+
+
+args = parser.parse_args()
+
+if not args.tag:
+    tag = getpass.getuser()
+else:
+    tag = args.tag
+
+msg_fmt = logging.Formatter(fmt='%(clienthostname)s %(tag)s: %(message)s')
+d = {'clienthostname': socket.getfqdn(), 'tag': tag}
+    
+syslogger = logging.getLogger('SyslogLogger')
+    
+pri = string_to_level(args.priority)
+syslogger.setLevel(pri)
+
+handler = logging.handlers.SysLogHandler(address=(args.server, args.port),
+                                         facility=19, socktype=socket.SOCK_STREAM)
+handler.setFormatter(msg_fmt)
+syslogger.addHandler(handler)
+
+syslogger.log(pri, args.msg, extra=d)
